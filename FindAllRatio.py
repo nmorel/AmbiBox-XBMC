3756b7756856b6e2f8faaaa3b21323bb6c76e82e
Nicolas Morel <nmr.morel@gmail.com>
2013-03-17 12:33:48 +0100

    updated script

--- FindAllRatio.py
+++ FindAllRatio.py
@@ -1,32 +1,44 @@
-import Media
 import os
+import codecs
+import Media
 
 media = Media.Media()
 
 ratios = dict()
 
-#paths = ['E:\\', 'F:\\', 'G:\\', 'H:\\']
-paths = ['F:\\']
-#paths = ['C:\\dev']
+paths = [u'C:\\dev']
 for path in paths:
     for root, dirs, files in os.walk(path):
         for name in files:
             if name.endswith((".mkv", ".avi", ".mp4", ".m2ts", ".ts")):
                 fullPath = os.path.join(root, name)
+
                 ratio = media.getAspectRatio(fullPath)
+
+                print "%s : %s" % (fullPath, ratio)
+
                 videosByRatio = ratios.get(ratio, list())
                 videosByRatio.append(fullPath)
                 ratios[ratio] = videosByRatio
 
+f = codecs.open("ratios.html", "w", "utf-8")
+f.write("<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\"></head><body>\n")
 
-print "%i ratios trouves" % (len(ratios))
 
-f = open('ratios.html', 'w')
+f.write("<p><u><b>%i different ratios found : </b></u></p>\n" % (len(ratios)))
+f.write("<ol>\n")
+for ratio in ratios.iterkeys():
+    videosByRatio = ratios.get(ratio)
+    f.write("<p><b>%s :</b> %i videos" % (ratio, len(videosByRatio)))
+f.write("</ol>")
 
+f.write("<p><u><b>Videos files by ratio :</b></u></p>")
 for ratio in ratios.iterkeys():
     f.write("<p><b>%s : </b></p>\n<ol>\n" % ratio)
     videosByRatio = ratios.get(ratio)
-    print "%s : %i occurences trouvees" % (ratio, len(videosByRatio))
     for video in videosByRatio:
         f.write("\t<li>%s</li>" % video)
     f.write("\n</ol><br>\n\n")
+
+f.write("</body></html>")
+f.close()
